b755abe77875c3ff07154f5a57d49fa5f711b51a
boiuW0Tm8 <30941775+boiuW0Tm8@users.noreply.github.com>
2018-07-06 21:28:30 -0400

    commit

---
+++ 02_package/main/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/boiuW0Tm8/goTime/02_package/stringutil"
+)
+
+func main() {
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
+	fmt.Println(stringutil.MyName)
+}
